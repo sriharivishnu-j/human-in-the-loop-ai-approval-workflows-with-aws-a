99864a3411095e6fb50ec08b7e2dba47944381df
Sriharivishnu Janakiraman <j.sriharivishnu@gmail.com>
2026-01-19 09:00:08 +0800

    Add backend/src/main/java/com/example/humanintheloop/HumanInTheLoopApplication.java

---
+++ backend/src/main/java/com/example/humanintheloop/HumanInTheLoopApplication.java
@@ -0,0 +1,13 @@
+package com.example.humanintheloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanInTheLoopApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HumanInTheLoopApplication.class, args);
+  }
+
+}
